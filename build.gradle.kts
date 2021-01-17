6230c24c8457ab6e4bcf75f31f2e96319007cf21
Mykhailo Kulak <mykhailo.kulak@olx.com>
2021-01-17 18:44:00 +0100

    switch to IR

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     kotlin("js") version "1.4.21"
 }
 
-
 val kotlinVersion = "1.4.21"
 val kjsVersion = "pre.138-kotlin"
+val reactVersion = "16.13.1"
 
 group = "me.user"
 version = "1.0"
@@ -18,14 +18,14 @@ repositories {
 dependencies {
     implementation("org.jetbrains:kotlin-react-router-dom:5.2.0-$kjsVersion-$kotlinVersion")
 
-    implementation("org.jetbrains:kotlin-react:16.13.1-$kjsVersion-$kotlinVersion")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.1-$kjsVersion-$kotlinVersion")
+    implementation("org.jetbrains:kotlin-react:$reactVersion-$kjsVersion-$kotlinVersion")
+    implementation("org.jetbrains:kotlin-react-dom:$reactVersion-$kjsVersion-$kotlinVersion")
     implementation("org.jetbrains:kotlin-styled:5.2.0-$kjsVersion-$kotlinVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-js:1.0.0-RC2")
-    implementation(npm("react", "16.13.1"))
-    implementation(npm("react-dom", "16.13.1"))
-    implementation(npm("react-is", "16.13.1"))
+    implementation(npm("react", reactVersion))
+    implementation(npm("react-dom", reactVersion))
+    implementation(npm("react-is", reactVersion))
     implementation(npm("styled-components", "5.2.0"))
     implementation(npm("inline-style-prefixer", "6.0.0"))
 
@@ -33,7 +33,7 @@ dependencies {
 }
 
 kotlin {
-    js(LEGACY) {
+    js(IR) {
         browser {
             binaries.executable()
             webpackTask {
